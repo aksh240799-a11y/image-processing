16fd0c884a93f69e0e47449cd5433af29d64c8fa
aksh240799-a11y <aksh240799@gmail.com>
2026-03-22 20:15:34 +0530

    Update Dataset Preparation for Transfer Learning.m

--- Dataset Preparation for Transfer Learning.m
+++ Dataset Preparation for Transfer Learning.m
@@ -1,3 +1,5 @@
+//Ground Truth Data//
+
 load /CourseData/aslVowels.mat gTruthVowels
 gTruthVowels
 gTruthVowels.LabelData
@@ -14,3 +16,19 @@ imLabeled = insertObjectAnnotation( ...
     im,"rectangle",bb,lab);
 imshow(imLabeled)
 
+// Splitting the dataset - Train, Validation and Test Data//
+load /CourseData/aslVowels.mat gTruthVowels
+[imds,bxds] = objectDetectorTrainingData(gTruthVowels);
+gtData = combine(imds,bxds);
+countEachLabel(bxds)
+gtShuffled = shuffle(gtData);
+
+numFiles = numpartitions(gtShuffled)
+numTrain = numFiles*0.7
+numVal = numFiles*0.1
+
+gtTrain = subset(gtShuffled,1:numTrain);
+gtVal = subset(gtShuffled,(numTrain+1):(numTrain+numVal));
+gtTest = subset(gtShuffled,(numTrain+numVal+1):numFiles);
+bxTrain = gtTrain.UnderlyingDatastores{2}
+trainCounts = countEachLabel(bxTrain)
